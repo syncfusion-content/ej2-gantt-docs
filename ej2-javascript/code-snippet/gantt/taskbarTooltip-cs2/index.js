0425d21a08216e1fc0685dca7fe3449b49146c6d
AjithkumarGopalakrishnan <ajithkumar.gopalakrishnan@syncfusion.com>
2023-08-23 17:28:06 +0530

    documentation(832369): sample added

---
+++ ej2-javascript/code-snippet/gantt/taskbarTooltip-cs2/index.js
@@ -0,0 +1,62 @@
+var GanttData = [
+        {
+            TaskID: 1,
+            TaskName: 'Project Initiation',
+            StartDate: new Date('04/02/2019'),
+            EndDate: new Date('04/21/2019'),
+            subtasks: [
+                { TaskID: 2, TaskName: 'Identify Site location', BaselineStartDate: new Date('04/02/2019'), BaselineEndDate: new Date('04/06/2019'), StartDate: new Date('04/02/2019'), Duration: 0, Progress: 50 },
+                { TaskID: 3, TaskName: 'Perform Soil test', BaselineStartDate: new Date('04/04/2019'), BaselineEndDate: new Date('04/09/2019'), StartDate: new Date('04/02/2019'), Duration: 4, Progress: 50 },
+                { TaskID: 4, TaskName: 'Soil test approval', BaselineStartDate: new Date('04/08/2019'), BaselineEndDate: new Date('04/12/2019'), StartDate: new Date('04/02/2019'), Duration: 4, Progress: 50 },
+            ]
+        },
+        {
+            TaskID: 5,
+            TaskName: 'Project Estimation',
+            StartDate: new Date('04/02/2019'),
+            EndDate: new Date('04/21/2019'),
+            subtasks: [
+                { TaskID: 6, TaskName: 'Develop floor plan for estimation', BaselineStartDate: new Date('04/04/2019'), BaselineEndDate: new Date('04/08/2019'), StartDate: new Date('04/04/2019'), Duration: 3, Progress: 50 },
+                { TaskID: 7, TaskName: 'List materials', BaselineStartDate: new Date('04/02/2019'), BaselineEndDate: new Date('04/04/2019'), StartDate: new Date('04/04/2019'), Duration: 3, Progress: 50 },
+                { TaskID: 8, TaskName: 'Estimation approval', BaselineStartDate: new Date('04/04/2019'), StartDate: new Date('04/04/2019'), Duration: 0, Progress: 50 }
+            ]
+        },
+    ];
+    var ganttChart = new ej.gantt.Gantt({
+        dataSource: GanttData,
+        height: '450px',
+        taskFields: {
+            id: 'TaskID',
+            name: 'TaskName',
+            startDate: 'StartDate',
+            duration: 'Duration',
+            progress: 'Progress',
+            baselineStartDate: "BaselineStartDate",
+            baselineEndDate: "BaselineEndDate",
+            child: 'subtasks',
+        },
+        editSettings: {
+            allowAdding: true,
+            allowEditing: true,
+            allowDeleting: true,
+            allowTaskbarEditing: true,
+            showDeleteConfirmDialog: true
+        },
+        renderBaseline: true,
+        baselineColor: 'red',
+        beforeTooltipRender: function (args) {
+            if (args.args.target.classList.contains('e-gantt-child-taskbar') || 
+				args.args.target.classList.contains('e-gantt-parent-taskbar') || 
+				args.args.target.classList.contains('e-taskbar-left-resizer') || 
+				args.args.target.classList.contains('e-taskbar-right-resizer')) 
+			{
+                args.cancel = true;
+            }
+        }
+    });
+ganttChart.appendTo('#Gantt');
+
+
+
+
+
