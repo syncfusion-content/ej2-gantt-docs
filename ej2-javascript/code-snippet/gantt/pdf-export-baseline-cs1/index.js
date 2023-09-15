1ee81ea13e759257a03a16f636e0f38e030724ca
pavithrashanmugamsf3943 <120448154+pavithrashanmugamsf3943@users.noreply.github.com>
2023-09-15 11:35:46 +0530

    Update index.js

--- ej2-javascript/code-snippet/gantt/pdf-export-baseline-cs1/index.js
+++ ej2-javascript/code-snippet/gantt/pdf-export-baseline-cs1/index.js
@@ -3,14 +3,6 @@ var clickHandler = function(args){
      ganttChart.pdfExport();
    }
 };
-
-var pdfQueryTaskbarInfo =  function(args){
-   if(args.data.Progress < 50 && !args.data.hasChildRecords) {
-        args.taskbar.progressColor = new ej.pdfexport.PdfColor(205, 92, 92);
-        args.taskbar.taskColor =  args.taskbar.taskBorderColor = new ej.pdfexport.PdfColor(240, 128, 128);
-    }
-};
-
 var ganttChart = new ej.gantt.Gantt({
     dataSource: GanttData,
     height: '450px',
