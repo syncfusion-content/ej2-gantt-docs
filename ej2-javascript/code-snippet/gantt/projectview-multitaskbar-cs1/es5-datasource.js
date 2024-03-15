527c29c2a58467121a3de2510c4550570b2c8c35
DhineshSF <dhinesh.govindharaji@syncfusion.com>
2024-03-15 11:29:55 +0530

    876168: Project view multi taskbar js

--- ej2-javascript/code-snippet/gantt/projectview-multitaskbar-cs1/es5-datasource.js
+++ ej2-javascript/code-snippet/gantt/projectview-multitaskbar-cs1/es5-datasource.js
@@ -1,24 +1,47 @@
-var GanttData = [
+var projectViewMultiTaskData = [
         {
             TaskID: 1,
-            TaskName: 'Project Initiation',
+            TaskName: 'Product Concept',
             StartDate: new Date('04/02/2019'),
             EndDate: new Date('04/21/2019'),
+            isExpand: false,
             subtasks: [
-                { TaskID: 2, TaskName: 'Identify Site location', StartDate: new Date('04/02/2019'), Duration: 4, Progress: 50 },
-                { TaskID: 3, TaskName: 'Perform Soil test', StartDate: new Date('04/02/2019'), Duration: 4, Progress: 50  },
-                { TaskID: 4, TaskName: 'Soil test approval', StartDate: new Date('04/02/2019'), Duration: 4, Progress: 50 },
+                { TaskID: 2, TaskName: 'Defining the product and its usage', StartDate: new Date('04/02/2019'), Duration: 3, Progress: 30 },
+                { TaskID: 3, TaskName: 'Defining target audience', StartDate: new Date('04/04/2019'), Duration: 3 },
+                { TaskID: 4, TaskName: 'Prepare product sketch and notes', StartDate: new Date('04/10/2019'), Duration: 3, Predecessor: "2", Progress: 30 },
             ]
         },
         {
             TaskID: 5,
-            TaskName: 'Project Estimation',
-            StartDate: new Date('04/02/2019'),
+            TaskName: 'Feedback and Testing',
+            StartDate: new Date('04/04/2019'),
             EndDate: new Date('04/21/2019'),
+            isExpand: true,
             subtasks: [
-                { TaskID: 6, TaskName: 'Develop floor plan for estimation', StartDate: new Date('04/04/2019'), Duration: 3, Progress: 50 },
-                { TaskID: 7, TaskName: 'List materials', StartDate: new Date('04/04/2019'), Duration: 3, Progress: 50 },
-                { TaskID: 8, TaskName: 'Estimation approval', StartDate: new Date('04/04/2019'), Duration: 3, Progress: 50 }
+                { TaskID: 6, TaskName: 'Internal testing and feedback', StartDate: new Date('04/04/2019'), Duration: 5, Progress: 30 },
+                { TaskID: 7, TaskName: 'Customer testing and feedback', StartDate: new Date('04/10/2019'), Duration: 7, Progress: 30 }
             ]
         },
+        {
+            TaskID: 8,
+            TaskName: 'Product Development',
+            StartDate: new Date('04/04/2019'),
+            EndDate: new Date('04/21/2019'),
+            isExpand: false,
+            subtasks: [
+                { TaskID: 9, TaskName: 'Important improvements', StartDate: new Date('04/04/2019'), Duration: 2, Progress: 30 },
+                { TaskID: 10, TaskName: 'Address any unforeseen issues', StartDate: new Date('04/06/2019'), Duration: 2, Progress: 30 }
+            ]
+        },
+        {
+            TaskID: 11,
+            TaskName: 'Final Product',
+            StartDate: new Date('04/04/2019'),
+            EndDate: new Date('04/21/2019'),
+            isExpand: true,
+            subtasks: [
+                { TaskID: 12, TaskName: 'Branding product', StartDate: new Date('04/06/2019'), Duration: 5 },
+                { TaskID: 13, TaskName: 'Marketing and pre-sales', StartDate: new Date('04/12/2019'), Duration: 10, Progress: 30 }
+            ]
+        }
     ];
